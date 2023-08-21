2412ce528514ce7675551984b58ba5f465daf0c5
grailian <grailians@gmail.com>
2023-08-20 23:53:38 -0400

    added tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import solid from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable Solid to support Solid JSX components.
-  integrations: [solid()],
+  integrations: [solid(), tailwind()],
 });
